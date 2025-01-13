b5af75e183c369f9975804813d92862168e2e7f5
Malay Ladu <malayladu@gmail.com>
2025-01-13 14:34:34 +0530

    Fix PHP Compatibility issue

--- lib/Events/CacheMissed.php
+++ lib/Events/CacheMissed.php
@@ -6,6 +6,11 @@ use PHPNomad\Events\Interfaces\Event;
 
 class CacheMissed implements Event
 {
+    public string $operation;
+    public array $context;
+    /* @var callable */
+    public $result;
+
     public function __construct($operation, $context, $result)
     {
         $this->operation = $operation;
